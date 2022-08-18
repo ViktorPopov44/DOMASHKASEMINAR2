e9da06460cbcb2b68149a769090af3590e79ac6b
ViktorPopov44 <110253043+ViktorPopov44@users.noreply.github.com>
2022-08-18 20:41:27 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,22 @@
-﻿Console.WriteLine("Введите координаты первой точки x, y, z");
-int x1 = Convert.ToInt32(Console.ReadLine());
-int y1 = Convert.ToInt32(Console.ReadLine());
-int z1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координаты второй точки x, y, z");
-int x2 = Convert.ToInt32(Console.ReadLine());
-int y2 = Convert.ToInt32(Console.ReadLine());
-int z2 = Convert.ToInt32(Console.ReadLine());
-
-Double findlenghe(int xa, int ya, int za, int xb, int yb, int zb)
-{
-
-Double a = Math.Sqrt((xb-xa)*(xb-xa)+(yb-ya)*(yb-ya)+(zb-za)*(zb-za));
-
-return a;
-}
-Double result=findlenghe( x1, y2, z1, x2, y1, z2);
-Console.Write("Длина отрезка =");
-Console.WriteLine(result);
+﻿
+int num, rem, sum = 0, temp;
+        Console.Write("Введите пятизначное число: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        temp = num;
+        while (Convert.ToBoolean(num))
+        {
+            rem = num % 10; 
+            num = num / 10; 
+            sum = sum * 10 + rem; 
+          
+        }
+        Console.WriteLine(sum);
+        if (temp == sum) 
+        {
+            Console.WriteLine("Это полинорм");
+        }
+        else
+        {
+            Console.WriteLine("Это не полинорм");
+        }
+        Console.ReadLine();
