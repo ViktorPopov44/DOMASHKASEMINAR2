5a9757427d23ceebfe2b3070d6a4585c4b70bf8a
Viktor <popov_viktor_007@mail.ru>
2022-08-18 16:11:32 +0300

    01

---
+++ zadacha001/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+bool numberday(int n);
+{
+    if ( 1 <= n < 6 )
+    {
+        Console.WriteLine("Деня не является выходным");
+    }
+    else 
+    Console.WriteLine("Это день выходной");
+}
+return n;
---
+++ zadacha002/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Числа в третьей степени от 0 до введенного числа   ");
+
+int[] TableSquares(int s)
+{
+    int[] array = new int[s];
+
+    for (int i = 1; i <= n; i++)
+    {
+        array[i - 1] = i * i * i;
+    }
+    return array;
+}
+int[] result = TableSquares(n);
+for (int i = 0; i < result.Length; i++)
+{
+    
+    Console.WriteLine(result[i]);
+}
